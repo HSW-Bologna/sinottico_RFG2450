328a1620dfc44fb53d360572fefcb2aaf654813e
Mattia Maldini <mattia512maldini@gmail.com>
2020-07-02 13:46:37 +0200

    Aggiunti dei controlli al popup di correzione

--- Sinottico/view/popups.py
+++ Sinottico/view/popups.py
@@ -1,14 +1,15 @@
 import PySimpleGUI as sg  # type: ignore
 import time
+import re
 
 
 def popupTimedCancel(title, text, delay, key="Cancel"):
     start = time.time()
-    layout = [[sg.Text(text)], [sg.Button(key, size=(16,1), key=key)]]
+    layout = [[sg.Text(text)], [sg.Button(key, size=(16, 1), key=key)]]
     window = sg.Window(title, layout, keep_on_top=True)
 
     while True:
-        event, _ = window.Read(timeout = 0.1)
+        event, _ = window.Read(timeout=0.1)
         if event == key:
             window.close()
             return key
@@ -19,14 +20,18 @@ def popupTimedCancel(title, text, delay, key="Cancel"):
             window.close()
             return None
 
+
 def delayPopup(delay):
     start = time.time()
     counter = 0
 
     while time.time() - start < delay:
-        layout = [[sg.Text("Attendere", size=(16,1), key="Text")]]
+        layout = [[sg.Text("Attendere", size=(16, 1), key="Text")]]
 
-        window = sg.Window("Attendere", layout, keep_on_top=True, size=(200, 80))
+        window = sg.Window("Attendere",
+                           layout,
+                           keep_on_top=True,
+                           size=(200, 80))
 
         while time.time() - start < delay:
             counter = (counter + 1) % 6
@@ -35,43 +40,71 @@ def delayPopup(delay):
             if event == None:
                 break
 
-            window["Text"].Update("Attendere" + "."*counter)
-        
+            window["Text"].Update("Attendere" + "." * counter)
+
         window.close()
 
 
-
 def adjustPopup(value):
+    oldvalue = str(value)
     layout = [[sg.Text("Compensare il valore rilevato:")],
-                [sg.Spin([x/10. for x in range(1, 10)], size=(3, 1), key="S"), sg.Text("Step")],
-                [sg.Button("-", size=(4,1), key="-"), sg.Input(str(value), size=(12,1), key="T"), sg.Button("+", size=(4,1), key="+")],
-                [sg.Button("OK", key="OK", bind_return_key=True)]]
-    window = sg.Window("Compensazione", layout, keep_on_top=True, return_keyboard_events=True)
+              [
+                  sg.Spin([x / 10. for x in range(5, 100, 5)],
+                          initial_value=0.5,
+                          size=(3, 1),
+                          key="S"),
+                  sg.Text("Step")
+              ],
+              [
+                  sg.Button("-", size=(4, 1), key="-"),
+                  sg.Input(str(value),
+                           size=(12, 1),
+                           change_submits=True,
+                           key="T"),
+                  sg.Button("+", size=(4, 1), key="+")
+              ], [sg.Button("OK", key="OK", bind_return_key=True)]]
+    window = sg.Window("Compensazione",
+                       layout,
+                       keep_on_top=True,
+                       return_keyboard_events=True)
 
     while True:
         event, values = window.Read()
 
-        if event in (None, "OK"):
-            window.close()
-            return float(window["T"].Get())
-        elif event in ["-", "Down:116", "minus:20"]:
-            x = float(values["T"])
-            s = values["S"]
-            if x - s >= 0:
-                window["T"].Update("{:.2f}".format(x - s))
-            else:
-                window["T"].Update("{:.2f}".format(0.))
-        elif event in ["+", "Up:111", "plus:21"]:
-            x = float(values["T"])
-            s = values["S"]
-            window["T"].Update("{:.2f}".format(x + s))
-
-
+        try:
+            if event in (None, "OK"):
+                window.close()
+                return float(window["T"].Get())
+            elif event == "T":
+                newvalue = re.sub("[^0-9]", "", values[event])
+                fvalue = float(newvalue)
+                window["T"].Update(newvalue)
+                oldvalue = newvalue
+            elif event in ["-", "Down:116", "minus:20"]:
+                x = float(values["T"])
+                s = values["S"]
+                if x - s >= 0:
+                    window["T"].Update("{:.2f}".format(x - s))
+                else:
+                    window["T"].Update("{:.2f}".format(0.))
+            elif event in ["+", "Up:111", "plus:21"]:
+                x = float(values["T"])
+                s = values["S"]
+                window["T"].Update("{:.2f}".format(x + s))
+        except ValueError:
+            window["T"].Update(oldvalue)
 
 
 def yesNoPopup(text):
-    layout = [[sg.Text(text)], [sg.Button("NO", size=(16,1), key="NO"), sg.Button("SI", size=(16,1), key="SI")]]
-    window = sg.Window("Conferma", layout, return_keyboard_events=True, keep_on_top=True)
+    layout = [[sg.Text(text)],
+              [
+                  sg.Button("NO", size=(16, 1), key="NO"),
+                  sg.Button("SI", size=(16, 1), key="SI")
+              ]]
+    window = sg.Window("Conferma",
+                       layout,
+                       return_keyboard_events=True,
+                       keep_on_top=True)
 
     while True:
         event, _ = window.Read()
