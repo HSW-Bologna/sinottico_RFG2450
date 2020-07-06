a4a364383e98a07056d5fa5d45841ce7262c2306
maldus512 <mattia512maldini@gmail.com>
2020-07-06 13:03:43 +0200

    Aggiunto un controllo sull'importazione dei moduli

--- Sinottico/utils/excelabstraction.py
+++ Sinottico/utils/excelabstraction.py
@@ -4,27 +4,31 @@ import os
 
 
 class CustomExcelWorkbookBecauseWindowsSucks:
+    def _openpyxl_init(self, filename):
+        self.native = False
+        from openpyxl import Workbook, load_workbook
+        self.wb = load_workbook(filename)
+        self.ws = self.wb.active
+    
     def __init__(self, filename):
         import platform
         if platform.system() in ["Windows", "windows"]:
-            import win32com.client
-            import pywintypes
             try:
-                self.native = True
-                self.excel = win32com.client.Dispatch('Excel.Application')
-                self.wb = self.excel.Workbooks.Open(
-                    os.path.join(os.getcwd(), filename))
-                self.ws = self.wb.Worksheets(1)
-            except pywintypes.com_error:
-                self.native = False
-                from openpyxl import Workbook, load_workbook
-                self.wb = load_workbook(filename)
-                self.ws = self.wb.active
+                import win32com.client
+                import pywintypes
+                try:
+                    self.native = True
+                    self.excel = win32com.client.Dispatch('Excel.Application')
+                    self.wb = self.excel.Workbooks.Open(
+                        os.path.join(os.getcwd(), filename))
+                    self.ws = self.wb.Worksheets(1)
+                except pywintypes.com_error:
+                    self._openpyxl_init(filename)
+            except ModuleNotFoundError:
+                    self._openpyxl_init(filename)
         else:
-            self.native = False
-            from openpyxl import Workbook, load_workbook
-            self.wb = load_workbook(filename)
-            self.ws = self.wb.active
+            self._openpyxl_init(filename)
+        print(self.native)
 
     def __setitem__(self, id, value):
         if self.native:
