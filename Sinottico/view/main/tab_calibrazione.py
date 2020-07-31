68cdc27c5d733a975cec0ec5f84bfd493694537c
maldus512 <mattia512maldini@gmail.com>
2020-07-31 17:54:36 +0200

    Prima bozza della tab di calibrazione

--- Sinottico/view/main/tab_calibrazione.py
+++ Sinottico/view/main/tab_calibrazione.py
@@ -3,4 +3,12 @@ from typing import List
 
 from .elements import Id
 
-tab : List[List[sg.Element]]= [[]]
+tab: List[List[sg.Element]] = [
+    [
+        sg.Input("", size=(48, 1), key=Id.LOADDATA),
+        sg.FileBrowse("File",
+                      file_types=(('Excel files', "*.xlsx"), ),
+                      size=(16, 1),
+                      key=Id.LOADDATABTN),
+    ]
+]
--- Sinottico/view/main/tab_dati.py
+++ Sinottico/view/main/tab_dati.py
@@ -4,49 +4,48 @@ from typing import List
 from .elements import Id
 
 
-tab : List[List[sg.Element]]= [
-        [
-            sg.Input("", size=(48, 1), key=Id.TEMPLATE),
-            sg.FileBrowse("Template",
-                          file_types=(('Excel files', "*.xlsx"), ),
-                          size=(16, 1),
-                          key=Id.TEMPBTN),
-        ],
-        [
-            sg.Input("", size=(48, 1), key=Id.DESTINATION),
-            sg.FolderBrowse("Destinazione", size=(16, 1), key=Id.DESTBTN),
-        ],
-        [
-            sg.Spin(["{:.2f}".format(x / 100.) for x in range(5, 155, 5)],
-                    initial_value="1.00",
-                    change_submits=True,
-                    size=(5, 1),
-                    key=Id.K),
-            sg.Text("Coefficiente K", size=(16, 1)),
-        ],
-        [
-            sg.Spin([x for x in range(1, 80)],
-                    initial_value="23",
-                    change_submits=True,
-                    size=(5, 1),
-                    key=Id.TEMP_LOW),
-            sg.Text("Temperatura bassa", size=(25, 1)),
-            sg.Spin([x for x in range(1, 80)],
-                    initial_value="43",
-                    change_submits=True,
-                    size=(5, 1),
-                    key=Id.TEMP_HIGH),
-            sg.Text("Temperatura alta", size=(25, 1)),
-        ],
-        [
-            sg.Button("Avvio Procedura", key=Id.AUTOTEST),
-            sg.Button("Riprendi", key=Id.RETRYAUTOTEST)
-        ],
-        [
-            sg.Multiline(disabled=True,
-                         autoscroll=True,
-                         key=Id.LOGAUTO,
-                         size=(68, 15))
-        ],
-    ]
-
+tab: List[List[sg.Element]] = [
+    [
+        sg.Input("", size=(48, 1), key=Id.TEMPLATE),
+        sg.FileBrowse("Template",
+                      file_types=(('Excel files', "*.xlsx"), ),
+                      size=(16, 1),
+                      key=Id.TEMPBTN),
+    ],
+    [
+        sg.Input("", size=(48, 1), key=Id.DESTINATION),
+        sg.FolderBrowse("Destinazione", size=(16, 1), key=Id.DESTBTN),
+    ],
+    [
+        sg.Spin(["{:.2f}".format(x / 100.) for x in range(5, 155, 5)],
+                initial_value="1.00",
+                change_submits=True,
+                size=(5, 1),
+                key=Id.K),
+        sg.Text("Coefficiente K", size=(16, 1)),
+    ],
+    [
+        sg.Spin([x for x in range(1, 80)],
+                initial_value="23",
+                change_submits=True,
+                size=(5, 1),
+                key=Id.TEMP_LOW),
+        sg.Text("Temperatura bassa", size=(25, 1)),
+        sg.Spin([x for x in range(1, 80)],
+                initial_value="43",
+                change_submits=True,
+                size=(5, 1),
+                key=Id.TEMP_HIGH),
+        sg.Text("Temperatura alta", size=(25, 1)),
+    ],
+    [
+        sg.Button("Avvio Procedura", key=Id.AUTOTEST),
+        sg.Button("Riprendi", key=Id.RETRYAUTOTEST)
+    ],
+    [
+        sg.Multiline(disabled=True,
+                     autoscroll=True,
+                     key=Id.LOGAUTO,
+                     size=(68, 15))
+    ],
+]
